40c4182c9ae50ff7c3bcbbc486ac27a69b278257
София Казачек <kazachek.sofi2004i@yandex.ru>
2025-12-10 23:21:40 +0300

    Part 2

--- laravel-react-coffee-shop-front/src/views/Login.jsx
+++ laravel-react-coffee-shop-front/src/views/Login.jsx
@@ -1,19 +1,54 @@
 import { Link } from "react-router-dom";
+import { useRef, useState } from "react";
+import { useStateContext } from "../contexts/ContextProviders";
+import axiosClient from "../axios-client";
 
 export default function Login() {
+    const { setUser, setToken } = useStateContext();
+    const emailRef = useRef();
+    const passwordRef = useRef();
+    const [errors, setErrors] = useState(null);
+
     const onSubmit = (ev) => {
-        ev.preventDefault()
+        ev.preventDefault();
+        const payload = {
+            email: emailRef.current.value,
+            password: passwordRef.current.value,
+        }
+        setErrors(null);
+        axiosClient.post('/login', payload)
+            .then(({ data }) => {
+                setUser(data.user)
+                setToken(data.token)
+            })
+            .catch(err => {
+                const response = err.response;
+                if (response && response.status === 422) {
+                    if (response.data.errors) {
+                        setErrors(response.data.errors);
+                    } else {
+                        setErrors({
+                            email: [response.data.message]
+                        });
+                    }
+                }
+            })
     }
 
     return (
         <div className="auth-form-wrapper fadeInDown">
             <h1 className="auth-title">Вход в систему</h1>
+            {errors && <div className="alert">
+                {Object.keys(errors).map(key => (
+                    <p key={key}>{errors[key][0]}</p>
+                ))}
+            </div>}
             <form onSubmit={onSubmit}>
                 <div className="input-group">
-                    <input type="email" placeholder="E-mail" />
+                    <input ref={emailRef} type="email" placeholder="E-mail" />
                 </div>
                 <div className="input-group">
-                    <input type="password" placeholder="Пароль" />
+                    <input ref={passwordRef} type="password" placeholder="Пароль" />
                 </div>
                 <button className="btn btn-block">Войти</button>
                 <p className="message">
--- laravel-react-coffee-shop-front/src/views/Signup.jsx
+++ laravel-react-coffee-shop-front/src/views/Signup.jsx
@@ -1,25 +1,59 @@
 import { Link } from "react-router-dom";
+import { useRef, useState } from "react";
+import { useStateContext } from "../contexts/ContextProviders";
+import axiosClient from "../axios-client";
 
 export default function Signup() {
+    const { setUser, setToken } = useStateContext();
+    const nameRef = useRef();
+    const emailRef = useRef();
+    const passwordRef = useRef();
+    const passwordConfirmRef = useRef();
+
+    const [errors, setErrors] = useState(null);
+
     const onSubmit = (ev) => {
-        ev.preventDefault()
+        ev.preventDefault();
+        const payload = {
+            name: nameRef.current.value,
+            email: emailRef.current.value,
+            password: passwordRef.current.value,
+            password_confirmation: passwordConfirmRef.current.value,
+        }
+        console.log(payload);
+        axiosClient.post('/signup', payload)
+            .then(({ data }) => {
+                setUser(data.user)
+                setToken(data.token)
+            })
+            .catch(err => {
+                const response = err.response;
+                if (response && response.status === 422) {
+                    setErrors(response.data.errors);
+                }
+            })
     }
 
     return (
         <div className="auth-form-wrapper fadeInDown">
             <h1 className="auth-title">Создание аккаунта</h1>
+            {errors && <div className="alert">
+                {Object.keys(errors).map(key => (
+                    <p key={key}>{errors[key][0]}</p>
+                ))}
+            </div>}
             <form onSubmit={onSubmit}>
                 <div className="input-group">
-                    <input type="text" placeholder="Имя" />
+                    <input ref={nameRef} type="text" placeholder="Имя" />
                 </div>
                 <div className="input-group">
-                    <input type="email" placeholder="E-mail" />
+                    <input ref={emailRef} type="email" placeholder="E-mail" />
                 </div>
                 <div className="input-group">
-                    <input type="password" placeholder="Пароль" />
+                    <input ref={passwordRef} type="password" placeholder="Пароль" />
                 </div>
                 <div className="input-group">
-                    <input type="password" placeholder="Подтверждение пароля" />
+                    <input ref={passwordConfirmRef} type="password" placeholder="Подтверждение пароля" />
                 </div>
                 <button className="btn btn-block">Зарегистрироваться</button>
                 <p className="message">
